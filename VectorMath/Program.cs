304a31b819c197a57a28fce147956bf0c78a40da
Parker Lange <parker03@ksu.edu>
2019-12-09 16:11:42 -0600

    Class 12/09

--- VectorMath/Program.cs
+++ VectorMath/Program.cs
@@ -7,6 +7,31 @@ namespace VectorMath
         static void Main(string[] args)
         {
             Console.WriteLine("Fun with vectors");
+            Vector3 one = new Vector3(3, 4, 5);
+            Vector3 two = new Vector3(4, 8, 0);
+            Vector3 three = new Vector3(4, 8, 0);
+
+            Console.WriteLine($"Static method Add: {Vector3.Add(one, two)}");
+            Console.WriteLine($"Object method Add: {one.Add(two)}");
+            Console.WriteLine($"Opertator: {one + two}");
+
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Subtraction");
+            Console.WriteLine($"Opertator: {one - two}");
+
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Multiplication");
+            Console.WriteLine($"Opertator: {2 * one}");
+
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Comparison");
+            Console.WriteLine($"Opertator: {one == two}");
+            Console.WriteLine($"Opertator: {two == three}");
+
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine($"Opertator: {one++}");
+            Console.WriteLine($"Magnitude: {~one}");
+
         }
     }
 }
